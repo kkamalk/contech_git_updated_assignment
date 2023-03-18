758113c06dcbfb40a990088fd848328febb38a92
kamal <krajakk6@gmail.com>
2023-03-18 20:12:58 +0530

    Fix print statement

--- my_script.py
+++ my_script.py
@@ -2,3 +2,10 @@
 # This is a simple Python script that prints out a message.
 
 print("Hello, world!")
+
+name = input("What is your name? ")
+print("Hello, " + name + "!")
+
+
+name = input("What is your name? ")
+print("Hello,", name + "!")
